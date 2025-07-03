6ed36862e868219bf09ec955461bee37f5ccd5a9
jahid Hassan <dev.jahidhassan@gmail.com>
2025-07-03 18:15:04 +0600

    object part three done

---
+++ object/object-three.js
@@ -0,0 +1,23 @@
+const computer={
+    brand:'Hp',
+    processor:'Intel',
+    ssd:500,
+    price:50000
+}
+// const keys= Object.keys(computer);
+// console.log(keys);
+
+const values = Object.values(computer);
+console.log(values);
+
+
+
+
+const profile = {
+    name:"Rohim",
+    age:25,
+    city:"Dhaka"
+}
+const profileKeys= Object.keys(profile);
+const hasName = profileKeys.includes('name');
+console.log(hasName);
